0c65ecea5e523520709f7cc353388d50b637ace3
sgt.hotshot@gmail.com <sgt.hotshot@gmail.com@c3375f4d-b2e8-b465-e945-3d41f0580d06>
2012-02-09 14:26:52 +0000

    Added a neuralNetwork class and min and Max class

---
+++ trunk/src/ai/heuristics/algorithms/minAndMax.java
@@ -0,0 +1,21 @@
+package ai.heuristics.algorithms;
+
+import gameboard.GameBoard;
+import ai.heuristics.Heuristics;
+
+public class minAndMax extends Heuristics{
+
+	
+	public minAndMax(){
+		
+	}
+	
+	
+	
+	@Override
+	public int choose(GameBoard board) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ trunk/src/ai/heuristics/algorithms/neuralNetwork.java
@@ -0,0 +1,23 @@
+package ai.heuristics.algorithms;
+
+import gameboard.GameBoard;
+import ai.heuristics.Heuristics;
+
+public class neuralNetwork extends Heuristics {
+
+	
+	public neuralNetwork(){
+		
+	}
+	
+	
+	
+	
+	
+	@Override
+	public int choose(GameBoard board) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
