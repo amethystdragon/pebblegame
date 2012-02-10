0d8a1de429ddaefcb7d2f3e2d09625ab51da1ab8
ggzaery@gmail.com <ggzaery@gmail.com@c3375f4d-b2e8-b465-e945-3d41f0580d06>
2012-02-10 04:09:48 +0000

    changed something....  :(

--- src/gameboard/GameWindow.java
+++ src/gameboard/GameWindow.java
@@ -3,7 +3,6 @@ package gameboard;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridLayout;
-import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
