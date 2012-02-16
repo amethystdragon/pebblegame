a508bd96fced8ba9ea3429e821bd9c9a1fabdaf1
ggzaery@gmail.com <ggzaery@gmail.com@c3375f4d-b2e8-b465-e945-3d41f0580d06>
2012-02-16 01:50:57 +0000

    Added functionality to the random AI. Added functionality to the game window to handle zero, one, or two players.

--- src/ai/Random.java
+++ src/ai/Random.java
@@ -4,14 +4,11 @@ import gameboard.GameBoard;
 
 public class Random extends AI{
 
+	private java.util.Random r = new java.util.Random();
+	
 	@Override
 	public int choose(GameBoard board) {
-		// TODO Auto-generated method stub
-		return 0;
+		super.choose(board);
+		return r.nextInt(3)+1;
 	}
-
-	
-	
-	
-	
 }
