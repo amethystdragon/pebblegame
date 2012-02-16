b22cbc2ccaeaf7ceab215a8d1babff9c019f0385
sgt.hotshot@gmail.com <sgt.hotshot@gmail.com@c3375f4d-b2e8-b465-e945-3d41f0580d06>
2012-02-16 00:40:22 +0000

    Update

--- src/ai/heuristics/Heuristics.java
+++ src/ai/heuristics/Heuristics.java
@@ -4,16 +4,7 @@ import gameboard.GameBoard;
 
 public abstract class Heuristics {
 	
-
-	public Heuristics(){
-		
-	}
 	
-	public int run(GameBoard board, String heuristic){
-		return 0;
-		
-	}
-	
-		
+			
 	public abstract int choose(GameBoard board);
 }
