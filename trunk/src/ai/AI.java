cacb4987639d2f28a19d36853951ebec6d1fbf8f
ggzaery@gmail.com <ggzaery@gmail.com@c3375f4d-b2e8-b465-e945-3d41f0580d06>
2012-02-16 11:48:45 +0000

    moved ais to their own packages

--- trunk/src/ai/AI.java
+++ trunk/src/ai/AI.java
@@ -2,13 +2,25 @@ package ai;
 
 import gameboard.GameBoard;
 
+import java.util.ArrayList;
+
 public abstract class AI {
 
-	protected GameBoard game;
+	protected ArrayList<Integer> path;
+	protected GameBoard currentBoard;
 	
+	
+	
+	public AI(){
+		path = new ArrayList<Integer>(100);
+	}
 	
 	public int choose(GameBoard board){
-		this.game = board;
+		path.add(board.getPebblesLeft());
+		this.currentBoard = board;
 		return 0;
 	}
+	
+	public abstract void gameOver(boolean win);
+
 }
