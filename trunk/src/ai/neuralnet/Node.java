caff9490824a4caf2587e5ec46ab8a7a1503c4ed
ggzaery@gmail.com <ggzaery@gmail.com@c3375f4d-b2e8-b465-e945-3d41f0580d06>
2012-02-16 11:49:00 +0000

    just moved

---
+++ trunk/src/ai/neuralnet/Node.java
@@ -0,0 +1,30 @@
+package ai.neuralnet;
+
+public class Node {
+
+	private int weight;
+	
+	private Node parent;
+	
+	public Node(Node parent){
+		weight = 0;
+		this.parent = parent;
+	}
+	
+	public Node(int i, Node[] minimaxNodes) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getWeight(){
+		return weight;
+	}
+	
+	public Node getParent(){
+		return parent;
+	}
+	
+	public void addWeight(int amount){
+		weight += amount;
+	}
+	
+}
